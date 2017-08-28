76093435a78095f5479f0ccfc79d8023e3ea551b
Tom Kadwill <tomkadwill@gmail.com>
2017-08-28 10:33:30 +0100

    Refactor into methods and classes

--- main.py
+++ main.py
@@ -1,33 +1,38 @@
 import os
 
-ONE_DRIVE_PATH = '/Users/thomaskadwill/OneDrive'
-GDRIVE_PATH = '/Users/thomaskadwill/Google Drive'
+class DocumentImporter(object):
+    ONE_DRIVE_PATH = '/Users/thomaskadwill/OneDrive'
+    GDRIVE_PATH = '/Users/thomaskadwill/Google Drive'
 
-def main():
-    print '1/4. Scanning OneDrive for new scanned files'
-    files_to_import = [f for f in os.listdir(ONE_DRIVE_PATH) if os.path.isfile(os.path.join(ONE_DRIVE_PATH, f))]
+    def run(self):
+        print '1/3. Importing the following files from OneDrive: %s' % self.scanned_image_files_to_import()
 
-    gdrive_directories = os.listdir(GDRIVE_PATH)
+        for file_to_import in self.scanned_image_files_to_import():
+            print '2/3. Which folder in GDrive would you like to import "%s"?' % file_to_import
+            print self.gdrive_directories(),
+            folder_to_import_to = raw_input()
 
-    scanned_image_files_to_import = []
-    for file_name in files_to_import:
-        if file_name.endswith('.pdf') or file_name.endswith('png'):
-            scanned_image_files_to_import.append(file_name)
+            one_drive_path_to_file = self.ONE_DRIVE_PATH + '/' + file_to_import
+            destination_file_path = self.GDRIVE_PATH + '/' + folder_to_import_to + '/' + file_to_import
 
-    print '2/4. Importing the following files: %s' % scanned_image_files_to_import
+            print 'Moving %s to %s' % (one_drive_path_to_file, destination_file_path)
+            os.rename(one_drive_path_to_file, destination_file_path)
 
-    for file_to_import in scanned_image_files_to_import:
-        print '3/4. Which folder in GDrive would you like to import "%s"?' % file_to_import
-        print gdrive_directories,
-        folder_to_import_to = raw_input()
+        print '3/3. Import complete.'
 
-        one_drive_path_to_file = ONE_DRIVE_PATH + '/' + file_to_import
-        destination_file_path = GDRIVE_PATH + '/' + folder_to_import_to + '/' + file_to_import
+    def files_to_import(self):
+        return [f for f in os.listdir(self.ONE_DRIVE_PATH) if os.path.isfile(os.path.join(self.ONE_DRIVE_PATH, f))]
 
-        print 'Moving %s to %s' % (one_drive_path_to_file, destination_file_path)
-        os.rename(one_drive_path_to_file, destination_file_path)
+    def gdrive_directories(self):
+        return os.listdir(self.GDRIVE_PATH)
 
-    print '4/4. Import complete.'
+    def scanned_image_files_to_import(self):
+        files = []
+        for file_name in self.files_to_import():
+            if file_name.endswith('.pdf') or file_name.endswith('png'):
+                files.append(file_name)
+
+        return files
 
 if __name__ == '__main__':
-    main()
+    DocumentImporter().run()
