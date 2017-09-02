916afa1bdebaecd95cb9b4704f75c9043ead0fdb
Tom Kadwill <tomkadwill@gmail.com>
2017-09-02 17:59:43 +0100

    started refactoring to make directory search recursive

--- main.py
+++ main.py
@@ -5,17 +5,35 @@ class DocumentImporter(object):
         print '1/3. Importing the following files from OneDrive: %s' % self.scanned_image_files_to_import()
 
         for file_to_import in self.scanned_image_files_to_import():
-            print '2/3. Which folder in GDrive would you like to import "%s"?' % file_to_import
-            print self.gdrive_directories(),
-            folder_to_import_to = raw_input()
+            folder_to_import_to = self.choose_file_to_import(file_to_import, self.gdrive_directories())
 
             one_drive_path_to_file = self.one_drive_path() + '/' + file_to_import
             destination_file_path = self.google_drive_path() + '/' + folder_to_import_to + '/' + file_to_import
 
-            print 'Moving %s to %s' % (one_drive_path_to_file, destination_file_path)
-            os.rename(one_drive_path_to_file, destination_file_path)
+            # TODO:
+            # make this a recursive function
+
+            full_folder_to_import_to = self.google_drive_path() + '/' + folder_to_import_to
+            print "Do you want to move '%s' to '%s'" % (file_to_import, full_folder_to_import_to)
+            move_to_current_dir = raw_input()
+
+            if (move_to_current_dir == 'y' or move_to_current_dir == 'yes'):
+                print 'Moving %s to %s' % (one_drive_path_to_file, destination_file_path)
+                # os.rename(one_drive_path_to_file, destination_file_path)
+            else:
+                directories = os.listdir(os.path.join(os.path.expanduser("~"), full_folder_to_import_to))
+                next_level_dir = self.choose_file_to_import(file_to_import, directories)
+
+                new_destination_path = destination_file_path + '/' + next_level_dir
+                print 'Moving %s to %s' % (one_drive_path_to_file, new_destination_path)
+                # os.rename(one_drive_path_to_file, new_destination_path)
 
         print '3/3. Import complete.'
+
+    def choose_file_to_import(self, file_to_import, directories):
+        print 'Which folder in GDrive would you like to import "%s"?' % file_to_import
+        print directories
+        return raw_input()
 
     def files_to_import(self):
         return [f for f in os.listdir(self.one_drive_path()) if os.path.isfile(os.path.join(self.one_drive_path(), f))]
